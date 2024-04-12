7518ab551242f19b262907c1b9f01c5fbb66c79b
theringmasterrr <thevagish@gmail.com>
2024-04-12 16:05:51 +0530

    roadmap decorations

--- components/pages/section4.js
+++ components/pages/section4.js
@@ -30,6 +30,30 @@ const Section4 = () => {
               width={500}
               className="absolute w-[18rem] -right-[1.3rem] -top-[12.8rem] "
             />
+
+            <Image
+              src={"/smolpaw1.svg"}
+              alt="paw"
+              height={500}
+              width={500}
+              className="absolute w-[6rem] left-[1rem] -top-[13rem] "
+            />
+
+            <Image
+              src={"/smolpaw2.svg"}
+              alt="paw"
+              height={500}
+              width={500}
+              className="absolute w-[6rem] left-[70rem] top-[7rem] "
+            />
+
+            <Image
+              src={"/arrow1.svg"}
+              alt="paw"
+              height={500}
+              width={500}
+              className="absolute w-[16rem] left-[48rem] top-[9rem] "
+            />
           </div>
           <Image
             src={"/bone5.svg"}
@@ -41,7 +65,7 @@ const Section4 = () => {
         </div>
         <div className="relative flex items-end justify-end ml-[20rem] mr-[14rem] h-[30rem]">
           <div className="relative flex flex-col bg-[#CFEEFF] w-[40rem] rounded-3xl py-20 px-[5.8rem] z-10">
-            <h3 className="text-5xl font-omnesblack uppercase pb-6">Stage 1</h3>
+            <h3 className="text-5xl font-omnesblack uppercase pb-6">Stage 2</h3>
             <p className="text-lg font-omnesreg font-semibold">
               Utilising smart contracts powered by Ethereum, DOGE20 is much more
               than a meme coin and brings passive earning potential to the
@@ -63,18 +87,34 @@ const Section4 = () => {
               width={500}
               className="absolute w-[18rem] -right-[1.3rem] -top-[12.8rem] "
             />
+
+            <Image
+              src={"/smolpaw3.svg"}
+              alt="paw"
+              height={500}
+              width={500}
+              className="absolute w-[6rem] right-[70rem] top-[1rem] "
+            />
+
+            <Image
+              src={"/arrow2.svg"}
+              alt="paw"
+              height={500}
+              width={500}
+              className="absolute w-[16rem] right-[55rem] top-[9rem] "
+            />
           </div>
           <Image
-            src={"/bone5.svg"}
+            src={"/bone6.svg"}
             alt="paw"
             height={500}
             width={500}
-            className="w-[16.25rem] absolute left-[34rem] -bottom-[7rem] z-0"
+            className="w-[15.25rem] absolute right-[8rem] -bottom-[10rem] z-0"
           />
         </div>
         <div className="relative flex items-end justify-start ml-[20rem] mr-[14rem] h-[30rem]">
           <div className="relative flex flex-col bg-[#CFEEFF] w-[40rem] rounded-3xl py-20 px-[5.8rem] z-10">
-            <h3 className="text-5xl font-omnesblack uppercase pb-6">Stage 1</h3>
+            <h3 className="text-5xl font-omnesblack uppercase pb-6">Stage 3</h3>
             <p className="text-lg font-omnesreg font-semibold">
               Utilising smart contracts powered by Ethereum, DOGE20 is much more
               than a meme coin and brings passive earning potential to the
@@ -96,18 +136,34 @@ const Section4 = () => {
               width={500}
               className="absolute w-[18rem] -right-[1.3rem] -top-[12.8rem] "
             />
+
+            <Image
+              src={"/smolpaw4.svg"}
+              alt="paw"
+              height={500}
+              width={500}
+              className="absolute w-[6rem] left-[65rem] top-[2rem] "
+            />
+
+            <Image
+              src={"/arrow3.svg"}
+              alt="paw"
+              height={500}
+              width={500}
+              className="absolute w-[16rem] left-[50rem] top-[9rem] "
+            />
           </div>
           <Image
-            src={"/bone5.svg"}
+            src={"/bone7.svg"}
             alt="paw"
             height={500}
             width={500}
-            className="w-[16.25rem] absolute left-[34rem] top-[7.8rem] z-0"
+            className="w-[16.25rem] absolute -left-[10rem] -top-[2rem] z-0"
           />
         </div>
         <div className="relative flex items-end justify-end ml-[20rem] mr-[14rem] h-[30rem]">
           <div className="relative flex flex-col bg-[#CFEEFF] w-[40rem] rounded-3xl py-20 px-[5.8rem] z-10">
-            <h3 className="text-5xl font-omnesblack uppercase pb-6">Stage 1</h3>
+            <h3 className="text-5xl font-omnesblack uppercase pb-6">Stage 4</h3>
             <p className="text-lg font-omnesreg font-semibold">
               Utilising smart contracts powered by Ethereum, DOGE20 is much more
               than a meme coin and brings passive earning potential to the
@@ -129,13 +185,20 @@ const Section4 = () => {
               width={500}
               className="absolute w-[18rem] -right-[1.3rem] -top-[12.8rem] "
             />
+            <Image
+              src={"/smolpaw5.svg"}
+              alt="paw"
+              height={500}
+              width={500}
+              className="absolute w-[6.5rem] right-[70rem] top-[4rem] "
+            />
           </div>
           <Image
-            src={"/bone5.svg"}
+            src={"/bone8.svg"}
             alt="paw"
             height={500}
             width={500}
-            className="w-[16.25rem] absolute left-[34rem] top-[7.8rem] z-0"
+            className="w-[16.25rem] absolute right-[30rem] -top-[4rem] z-0"
           />
         </div>
       </div>
