82dc72f93f75493e99f6b256c944fed6dc79bac4
theringmasterrr <thevagish@gmail.com>
2024-05-03 19:23:33 +0530

    some optimizations

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 import "./globals.css";
 import Header from "@/components/header/Header"
-import Section1 from "@/components/pages/Hero"
-import Section2 from "@/components/pages/SubHero"
-import Section3 from "@/components/pages/FeaturedIn"
-import Section4 from "@/components/pages/Roadmap"
-import Section5 from "@/components/pages/Referral";
-import Section6 from "@/components/pages/Tokenomics";
-import Section7 from "@/components/pages/BuyMe"
-import Section8 from "@/components/pages/BuyMe2"
-import Section9 from "@/components/pages/RewardsCalculator";
+import Section1 from "@/components/homepage/Hero"
+import Section2 from "@/components/homepage/SubHero"
+import Section3 from "@/components/homepage/FeaturedIn"
+import Section4 from "@/components/homepage/Roadmap"
+import Section5 from "@/components/homepage/Referral";
+import Section6 from "@/components/homepage/Tokenomics";
+import Section7 from "@/components/homepage/BuyMe"
+import Section8 from "@/components/homepage/BuyMe2"
+import Section9 from "@/components/homepage/RewardsCalculator";
 import Section10 from "@/components/footer/Footer";
 
 
